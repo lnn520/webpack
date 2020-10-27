cfe8cf2652366217b6a0aea5e7e8135f10e58e8d
lnn520 <335341543@qq.com>
2020-10-27 23:27:47 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,24 @@
-var path = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+var path = require('path');
+const { Template } = require("webpack");
 module.exports = {
     mode:'development',
     entry: './src/index.js',
     output: {
     filename: '[name].[contenthash].js'
-  }
-}
+  },
+  plugins:[new HtmlWebpackPlugin(
+      {
+          title:'MY APP',
+          template:'./src/assets/index.html'
+      }
+  )],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      },
+    ],
+  },
+};
