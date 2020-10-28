60c675ad4b80c973da3ce31fa69ff35ed1540423
lnn520 <335341543@qq.com>
2020-10-28 23:20:56 +0800

    准备上传了

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
-import './x.css'
-console.log('hi')
+import './x.scss'
+import png from './assets/1.png'
+import './y.less'
+import './z.styl'
+const div = document.getElementById('app')
+console.log(png)
+div.innerHTML = `
+<img src ="${png}">
+`
+const button = document.createElement('button')
+button.innerHTML = '懒加载'
+button.onclick = ()=>{
+    const promise = import('./lazy')
+    promise.then((module)=>{
+        const fn = module.default
+        fn()
+    },()=>{})
+
+}
+div.appendChild(button)
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -12,5 +12,39 @@ module.exports = {
       {
           title:'MY APP',
           template:'./src/assets/index.html'
-      }),]
+      }),
+    ],
+    module: {
+        rules: [
+            {
+                test:/\.(png|svg|jpg|gif)$/,
+                use:["file-loader"]
+            },
+            // 有问题
+            {
+                test: /\.less$/,
+                use: ['style-loader','css-loader','less-loader'], // 将 Less 文件编译为 CSS 文件
+              },
+            {
+                     test: /\.styl$/,
+                     //把loader:改为use:
+                     use: ['style-loader','css-loader','stylus-loader'], // 将 Less 文件编译为 CSS 文件 
+                   },
+          {
+            test: /\.scss$/i,
+            use: [
+              // 将 JS 字符串生成为 style 节点
+              'style-loader',
+              // 将 CSS 转化成 CommonJS 模块
+              'css-loader',
+              // 将 Sass 编译成 CSS
+            //   'sass-loader',
+              {
+                  loader:"sass-loader",
+                  options:{implementation:require('dart-sass')}
+              }
+            ], 
+          },
+        ],
+      },
 };
